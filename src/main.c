84a478291a7fc13d032abfdbac2804b4cc7541f4
Alexey Samoylov <trollex2013@gmail.com>
2021-03-31 22:17:25 +0300

    Remade dynamic lib build and usage

--- src/main.c
+++ src/main.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
 #include "numseq.h"
+#include <dlfcn.h>
 
 int main() {
-    printf("Homework 2\n");
+    printf("Simple function call: ");
     if (numseq() == 1) {
-        printf("Consistent realisation");
-    } else if (numseq() == 2) {
-        printf("Parallel realisation");
+        printf("Consistent realisation\n");
     }
+
+    void *library = 0;
+    int (*numseq_p)();
+
+    library = dlopen("./lib_num_sequence_parallel.so", RTLD_NOW);
+    if (!library) {
+        printf("%s", "Библиотека не найдена");
+        return CODE_ERROR;
+    }
+    *(void **) (&numseq_p) = dlsym(library, "numseq");
+    int a = (*numseq_p)();
+
+    printf("dlsym function call: ");
+    if (a == 2) {
+        printf("Parallel realisation\n");
+    }
+    dlclose(library);
     return 0;
 }
