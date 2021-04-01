72ad8693c06b6689f0a41d1baccc726a9c537cda
Alexey Samoylov <trollex2013@gmail.com>
2021-04-01 17:16:39 +0300

    First version

--- lib_num_sequence/src_parallel/numseq.c
+++ lib_num_sequence/src_parallel/numseq.c
@@ -1,5 +1,166 @@
+#include <stddef.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <wait.h>
 #include "numseq.h"
 
-int numseq() {
-    return 2;
+int create_ranges(int count, size_t filesize, size_t *ranges) {
+    size_t proc_range = filesize / count;
+
+    if (ranges == NULL) {
+        return CODE_ERROR;
+    }
+    for (int i = 1; i < count; i++) {
+        ranges[i] = ranges[i - 1] + proc_range;
+    }
+    ranges[count] = filesize;
+    return 0;
+}
+
+int fork_calculations(int *processes, int process_count) {
+    if (processes == NULL) {
+        return CODE_ERROR;
+    }
+    for (int i = 0; i < process_count; i++) {
+        int current_id = fork();
+        if (current_id == -1) {
+            return CODE_ERROR;
+        }
+        if (current_id == 0) {
+            return i;
+        }
+        processes[i] = current_id;
+    }
+    return getpid();
+}
+
+void free_ranges_and_processes(size_t *ranges, int *processes) {
+    if (ranges) {
+        free(ranges);
+    }
+    if (processes) {
+        free(processes);
+    }
+}
+
+int max_number_sequence(const char *arr, subsequence *subseq_max, size_t arr_length) {
+    if (arr == NULL || subseq_max == NULL) {
+        return CODE_ERROR;
+    }
+
+    size_t process_count = sysconf(_SC_NPROCESSORS_ONLN);
+
+    size_t *ranges = (size_t *) calloc(process_count + 1, sizeof(size_t));
+    create_ranges(process_count, arr_length, ranges);
+
+    int pipes[MAX_PROCESS_COUNT][2];
+    memset(pipes, 0, MAX_PROCESS_COUNT * 2);
+
+    for (int i = 0; i < process_count; i++) {
+        pipe(pipes[i]);
+    }
+
+    int *processes = (int *) calloc(process_count, sizeof(pid_t));
+    int current_id = fork_calculations(processes, process_count);
+
+    // CHILD PROCESS
+    if (getpid() != current_id) {
+        close(pipes[current_id][0]);
+
+        subsequence subseq_cur = {0, 0, 0};
+        subsequence subseq_max_child = {0, ranges[current_id + 1], 0};
+        subsequence subseq_first = {ranges[current_id], 0, 0};  // структура, хранящая subseq начинающийся с 0 индекса
+
+        for (size_t i = ranges[current_id]; i <= ranges[current_id + 1]; i++) {
+            char ch = arr[i];
+            if ((ch >= '0') && (ch <= '9')) {
+                if (subseq_cur.len == 0) {
+                    subseq_cur.start = i;
+                }
+                subseq_cur.len++;
+                if (subseq_cur.len > subseq_max_child.len) {
+                    subseq_max_child.start = subseq_cur.start;
+                    subseq_max_child.end = subseq_cur.start + subseq_cur.len;
+                    subseq_max_child.len = subseq_cur.len;
+                }
+            } else {
+                if (subseq_cur.start == ranges[current_id]) {
+                    subseq_first.end = subseq_cur.end;
+                    subseq_first.len = subseq_cur.len;
+                }
+                subseq_cur.len = 0;
+            }
+        }
+        write(pipes[current_id][1], &subseq_max_child, sizeof(subsequence));
+        write(pipes[current_id][1], &subseq_first, sizeof(subsequence));
+
+        int ends_with_digit = 0;
+        if (subseq_cur.len > 0) {
+            ends_with_digit = 1;
+            write(pipes[current_id][1], &ends_with_digit, sizeof(int));
+            write(pipes[current_id][1], &subseq_cur, sizeof(subsequence));
+        } else {
+            write(pipes[current_id][1], &ends_with_digit, sizeof(int));
+        }
+
+        close(pipes[current_id][1]);
+        free_ranges_and_processes(ranges, processes);
+        exit(0);
+    }
+
+    // структура для хранения подпоследовательностей, лежащих на нескольких отрезках
+    subsequence subseq_global = {0, 0, 0};
+
+    // PARENT PROCESS
+    for (int i = 0; i < process_count; i++) {
+        close(pipes[i][1]);
+
+        if (waitpid(processes[i], NULL, 0) == -1) {
+            return CODE_ERROR;
+        }
+
+        subsequence subseq_child_max = {0, 0, 0};
+        read(pipes[i][0], &subseq_child_max, sizeof(subsequence));
+
+        if (subseq_child_max.len > subseq_max->len) {
+            subseq_max->start = subseq_child_max.start;
+            subseq_max->end = subseq_child_max.end;
+            subseq_max->len = subseq_child_max.len;
+        }
+
+        subsequence subseq_child_first = {0, 0, 0};
+        read(pipes[i][0], &subseq_child_first, sizeof(subsequence));
+        int ends_with_digit = 0;
+        read(pipes[i][0], &ends_with_digit, sizeof(int));
+
+        subsequence subseq_child_last = {0, 0, 0};
+        if (ends_with_digit) {
+            read(pipes[i][0], &subseq_child_last, sizeof(subsequence));
+        }
+
+        if (subseq_global.len > 0) {  // если в прошлых процессах уже была незаконченная subseq
+            // то добавляем продолжение незаконченной subseq
+            subseq_global.len += subseq_child_first.len;
+            subseq_global.end = subseq_child_first.end;
+            // если незаконченая subseq больше максимальной subseq
+            if (subseq_global.len > subseq_max->len) {
+                subseq_max->start = subseq_global.start;
+                subseq_max->end = subseq_global.end;
+                subseq_max->len = subseq_global.len;
+            }
+            // если незаконченная subseq закончилась
+            if ((subseq_global.end != subseq_child_last.end && ends_with_digit) || !ends_with_digit) {
+                subseq_global = (const subsequence) {0};  // обнуляем
+            }
+        } else if (ends_with_digit) {
+            subseq_global.start = subseq_child_last.start;
+            subseq_global.end = subseq_child_last.end;
+            subseq_global.len = subseq_child_last.len;
+        }
+        close(pipes[i][0]);
+    }
+
+    free_ranges_and_processes(ranges, processes);
+    return CODE_SUCCESS;
 }
--- src/main.c
+++ src/main.c
@@ -1,28 +1,120 @@
 #include <stdio.h>
-#include "numseq.h"
 #include <dlfcn.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include "numseq.h"
 
-int main() {
-    printf("Simple function call: ");
-    if (numseq() == 1) {
-        printf("Consistent realisation\n");
-    }
+#define TESTS_COUNT 1
 
-    void *library = 0;
-    int (*numseq_p)();
-
-    library = dlopen("./lib_num_sequence_parallel.so", RTLD_NOW);
-    if (!library) {
-        printf("%s", "Библиотека не найдена");
+int load_file_to_arr(FILE *file, char *arr, size_t count) {
+    if (file == NULL) {
         return CODE_ERROR;
     }
-    *(void **) (&numseq_p) = dlsym(library, "numseq");
-    int a = (*numseq_p)();
 
-    printf("dlsym function call: ");
-    if (a == 2) {
-        printf("Parallel realisation\n");
+    for (int i = 0; i < count; i++) {
+        char buffer = '\0';
+        if (!fscanf(file, "%c", &buffer)) {
+            return CODE_ERROR;
+        }
+        arr[i] = buffer;
     }
-    dlclose(library);
-    return 0;
+
+    if (fclose(file) != CODE_SUCCESS) {
+        return CODE_ERROR;
+    }
+
+    return CODE_SUCCESS;
+}
+
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        return CODE_ERROR;
+    }
+
+    FILE *file = fopen(argv[1], "r");
+    if (!file) {
+        return CODE_ERROR;
+    }
+
+    struct stat file_info;
+    fstat(fileno(file), &file_info);
+    if ((file_info.st_mode & S_IRUSR) == 0 || file_info.st_size == 0) {
+        return CODE_ERROR;
+    }
+
+    char *arr_chars;
+
+    long l1dcls = sysconf(_SC_LEVEL1_DCACHE_LINESIZE);
+
+    if (l1dcls == -1) {
+        l1dcls = sizeof(void *);
+    }
+
+    if (posix_memalign((void **) &arr_chars, l1dcls, sizeof(char) * file_info.st_size)) {
+        return CODE_ERROR;
+    }
+
+    if (load_file_to_arr(file, arr_chars, file_info.st_size) == -1) {
+        return CODE_ERROR;
+    }
+
+
+    double elapsed[TESTS_COUNT] = {0};
+    double average[2] = {0};  // [0] for consistent, [1] for parallel
+    subsequence subseq_max_c = {0, 0, 0};
+    subsequence subseq_max_p = {0, 0, 0};
+
+    for (int i = 0; i < TESTS_COUNT; i++) {
+        struct timespec start, finish;
+        subseq_max_c = (const subsequence) {0};
+        if (clock_gettime(_POSIX_MONOTONIC_CLOCK, &start) == -1) {
+            return CODE_ERROR;
+        }
+        max_number_sequence(arr_chars, &subseq_max_c, file_info.st_size);
+        if (clock_gettime(_POSIX_MONOTONIC_CLOCK, &finish) == -1) {
+            return CODE_ERROR;
+        }
+
+        elapsed[i] = (double) (finish.tv_sec - start.tv_sec);
+        elapsed[i] += (double) (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+        average[0] += elapsed[i];  // add to consistent
+
+        void *library = 0;
+        int (*max_number_sequence)(const char *arr, subsequence *subseq_max, size_t arr_length);
+        library = dlopen("./lib_num_sequence_parallel.so", RTLD_NOW);
+        if (!library) {
+            printf("%s", "Library not found");
+            return CODE_ERROR;
+        }
+        *(void **) (&max_number_sequence) = dlsym(library, "max_number_sequence");
+        subseq_max_p = (const subsequence) {0};
+        if (clock_gettime(_POSIX_MONOTONIC_CLOCK, &start) == -1) {
+            return CODE_ERROR;
+        }
+        (*max_number_sequence)(arr_chars, &subseq_max_p, file_info.st_size);
+        if (clock_gettime(_POSIX_MONOTONIC_CLOCK, &finish) == -1) {
+            return CODE_ERROR;
+        }
+        dlclose(library);
+        elapsed[i] = (double) (finish.tv_sec - start.tv_sec);
+        elapsed[i] += (double) (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+        average[1] += elapsed[i];  // add to parallel
+    }
+
+    if (subseq_max_c.start == subseq_max_p.start && subseq_max_c.end == subseq_max_p.end &&
+        subseq_max_c.len == subseq_max_p.len) {
+        printf("Answer - start: %lu, end: %lu, len: %lu\n", subseq_max_c.start, subseq_max_c.end, subseq_max_c.len);
+    } else {
+        printf("Consistent and parallel implementation have different answers!\n");
+    }
+    average[0] = average[0] / TESTS_COUNT;
+    average[1] = average[1] / TESTS_COUNT;
+    printf("Consistent series average time: %lf\n", average[0]);
+    printf("Parallel series average time: %lf\n", average[1]);
+
+    free(arr_chars);
+    return CODE_SUCCESS;
 }
