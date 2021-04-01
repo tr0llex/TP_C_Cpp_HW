7eeeb39dc1be44ad2036a81d76aa6e2996ccb647
Alexey Samoylov <trollex2013@gmail.com>
2021-04-01 17:16:39 +0300

    First version

--- lib_num_sequence/include/numseq.h
+++ lib_num_sequence/include/numseq.h
@@ -3,7 +3,14 @@
 
 #define CODE_ERROR 1
 #define CODE_SUCCESS 0
+#define MAX_PROCESS_COUNT 48
 
-int numseq();
+typedef struct subsequence {
+    size_t start;
+    size_t end;
+    size_t len;
+} subsequence;
+
+int max_number_sequence(const char *arr, subsequence *subseq_max, size_t arr_length);
 
 #endif  // LIB_NUM_SEQUENCE_INCLUDE_NUMSEQ_H_
